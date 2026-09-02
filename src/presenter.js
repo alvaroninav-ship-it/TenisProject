c7b320d7388556c3d7097ca3840a50cb8649db7f
Alvaro Nina <alvaro.nina.v@ubc.edu.bo>
2026-09-03 00:54:11 +0200

    feat: Advantages y deuces controlados

--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,6 @@
 import sumar from "./sumador";
 import marcador from "./marcador";
-
+import verificador from "./verificador";
 const resultado = document.querySelector("#resultado-div");
 const button_player1= document.querySelector("#primer-jugador-anota");
 const button_player2= document.querySelector("#segundo-jugador-anota");
@@ -8,10 +8,18 @@ let number_player1=0;
 let number_player2=0;
 button_player1.addEventListener("click", () => {
     number_player1=sumar(number_player1);
+    if (verificador(number_player1,number_player2)){
+        number_player1=40;
+        number_player2=40;
+    }
     resultado.innerHTML=marcador(number_player1,number_player2);
 
 });
 button_player2.addEventListener("click", () => {
     number_player2=sumar(number_player2);
+    if (verificador(number_player1,number_player2)){
+        number_player1=40;
+        number_player2=40;
+    }
     resultado.innerHTML=marcador(number_player1,number_player2);
 });
---
+++ src/verificador.js
@@ -0,0 +1,8 @@
+function verificador(a,b) {
+  if (a==50 && b==50){
+    return true;
+  }
+  return false;
+}
+
+export default verificador;
